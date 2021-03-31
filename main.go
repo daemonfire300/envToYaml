fd7bf580ceb3a7b519befd7b8f0ae67643d3f0c9
Julius Foitzik <info@accountr.eu>
2021-03-31 11:04:20 +0200

    Make results ordered and therefore stable

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/joho/godotenv"
 )
+
+type orderedMap [][2]string
+
+func (a orderedMap) Len() int           { return len(a) }
+func (a orderedMap) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a orderedMap) Less(i, j int) bool { return a[i][0] < a[j][0] }
 
 func main() {
 	in, out := os.Args[1], os.Args[2]
@@ -26,8 +33,13 @@ func main() {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+	oMap := make(orderedMap, 0)
 	for k, v := range env {
-		_, err = fmt.Fprintf(outF, "%s: \"%s\"\n", strings.ToLower(k), v)
+		oMap = append(oMap, [2]string{k, v})
+	}
+	sort.Sort(oMap)
+	for _, e := range oMap {
+		_, err = fmt.Fprintf(outF, "%s: \"%s\"\n", strings.ToLower(e[0]), e[1])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(-1)
